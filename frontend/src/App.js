2fc7aee0b9ff29d2e376af1cb7e97de508ceaee6
Felix-fale <falefelix23@gmail.com>
2023-04-20 13:53:01 +0000

    feat: add Login Page Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./Routes";
 
 function App() {
-  return <div>app</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
