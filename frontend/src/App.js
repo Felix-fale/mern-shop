a4ed6af0e30814a73d9c3e95bc400ccc3da651af
Felix-fale <falefelix23@gmail.com>
2023-04-20 21:35:25 +0000

    feat: add Signup Page Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoginPage } from "./Routes";
+import { LoginPage, SignupPage } from "./Routes";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
