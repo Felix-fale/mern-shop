c782307dc7a5eb4295fd8c92f7317aeafa010213
Felix-fale <falefelix23@gmail.com>
2023-05-02 14:10:30 +0000

    feat: add sendMail file content

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -6,14 +6,18 @@ const { upload } = require("../multer");
 const ErrorHandler = require("../utils/ErrorHandler");
 // const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const fs = require("fs");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 // const sendMail = require("../utils/sendMail");
 // const sendToken = require("../utils/jwtToken");
 // const { isAuthenticated } = require("../middleware/auth");
 // const user = require("../model/user");
+const crypto = require("crypto");
 
 router.post("/create-user", upload.single("file"), async (req, res, next) => {
   try {
+    const secret = crypto.randomBytes(64).toString("hex");
+
+    console.log(secret);
     const { name, email, password } = req.body;
     const userEmail = await User.findOne({ email });
 
@@ -41,29 +45,29 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
       avatar: fileUrl,
     };
 
-    const newUser = await User.create(user);
-    res.status(201).json({
-      success: true,
-      newUser,
-    });
+    // const newUser = await User.create(user);
+    // res.status(201).json({
+    //   success: true,
+    //   newUser,
+    // });
 
-    // const activationToken = createActivationToken(user);
+    const activationToken = createActivationToken(user);
 
-    // const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
 
-    // try {
-    //   await sendMail({
-    //     email: user.email,
-    //     subject: "Activate your account",
-    //     message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
-    //   });
-    //   res.status(201).json({
-    //     success: true,
-    //     message: `please check your email:- ${user.email} to activate your account!`,
-    //   });
-    // } catch (error) {
-    //   return next(new ErrorHandler(error.message, 500));
-    // }
+    try {
+      // await sendMail({
+      //   email: user.email,
+      //   subject: "Activate your account",
+      //   message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
+      // });
+      // res.status(201).json({
+      //   success: true,
+      //   message: `please check your email:- ${user.email} to activate your account!`,
+      // });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
   } catch (error) {
     return next(new ErrorHandler(error.message, 400));
   }
@@ -118,11 +122,11 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
 // });
 
 // create activation token
-// const createActivationToken = (user) => {
-//   return jwt.sign(user, process.env.ACTIVATION_SECRET, {
-//     expiresIn: "5m",
-//   });
-// };
+const createActivationToken = (user) => {
+  return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+    expiresIn: "5m",
+  });
+};
 
 // // activate user
 // router.post(
