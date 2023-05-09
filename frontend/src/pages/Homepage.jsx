74ea7c990db33756b5782a9bc476643c3ff1e699
Felix-fale <falefelix23@gmail.com>
2023-05-09 22:23:32 +0000

    feat: add the Footer component content

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -6,6 +6,7 @@ import BestDeals from "../components/Route/BestDeals/BestDeals.jsx";
 import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct.jsx";
 import Events from "../components/Events/Events.jsx";
 import Sponsored from "../components/Route/Sponsored.jsx";
+import Footer from "../components/Layout/Footer.jsx";
 
 function Homepage(props) {
   return (
@@ -17,6 +18,7 @@ function Homepage(props) {
       <Events />
       <FeaturedProduct />
       <Sponsored />
+      <Footer />
     </div>
   );
 }
