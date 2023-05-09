87cf17fabcef04fd72fce2de965d299620293956
Felix-fale <falefelix23@gmail.com>
2023-05-09 08:41:19 +0000

    feat: add the content of the Hero component

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "../components/Layout/Header.jsx";
+import Hero from "../components/Route/Hero/Hero.jsx";
 
 function Homepage(props) {
   return (
     <div>
       <Header activeHeading={1} />
-      <div>Homepage</div>
+      <Hero />
     </div>
   );
 }
