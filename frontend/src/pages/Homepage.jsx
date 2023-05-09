fd982c001a483f74789d474984e2bdfdc420514c
Felix-fale <falefelix23@gmail.com>
2023-05-09 09:31:13 +0000

    feat: add the content of the Categories component

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Layout/Header.jsx";
 import Hero from "../components/Route/Hero/Hero.jsx";
+import Categories from "../components/Route/Categories/Categories.jsx";
 
 function Homepage(props) {
   return (
     <div>
       <Header activeHeading={1} />
       <Hero />
+      <Categories />
     </div>
   );
 }
