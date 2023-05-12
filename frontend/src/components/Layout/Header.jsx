1e0a2af41dfe24defad4c0cdbc4ad789d55b842d
Felix-fale <falefelix23@gmail.com>
2023-05-12 17:43:52 +0000

    feat: add the content of the Wishlist component

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -17,6 +17,7 @@ import DropDown from "./DropDown.jsx";
 import { backend_url } from "../../server";
 import { loadUser } from "../../redux/actions/user";
 import Cart from "../cart/Cart.jsx";
+import Wishlist from "../Wishlist/Wishlist.jsx";
 
 function Header({ activeHeading }) {
   const { isAuthenticated, user } = useSelector((state) => state.user);
@@ -166,7 +167,7 @@ function Header({ activeHeading }) {
               >
                 <AiOutlineHeart size={30} color="rgb(255 255 255 / 83%)" />
                 <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center">
-                  0{/* {wishlist && wishlist.length} */}
+                  3{/* {wishlist && wishlist.length} */}
                 </span>
               </div>
             </div>
@@ -208,9 +209,9 @@ function Header({ activeHeading }) {
             {openCart ? <Cart setOpenCart={setOpenCart} /> : null}
 
             {/* wishlist popup */}
-            {/* {openWishlist ? (
+            {openWishlist ? (
               <Wishlist setOpenWishlist={setOpenWishlist} />
-            ) : null} */}
+            ) : null}
           </div>
         </div>
       </div>
