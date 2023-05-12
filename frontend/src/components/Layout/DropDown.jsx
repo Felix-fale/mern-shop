59104bc3bee54ce6f560bcb24ceeeb3fc61fda81
Felix-fale <falefelix23@gmail.com>
2023-05-12 00:36:29 +0000

    feat: improvement all app logic

--- frontend/src/components/Layout/DropDown.jsx
+++ frontend/src/components/Layout/DropDown.jsx
@@ -6,7 +6,7 @@ function DropDown({ categoriesData, setDropDown }) {
   const submitHandle = (i) => {
     navigate(`/products?category=${i.title}`);
     setDropDown(false);
-    window.location.reload();
+    // window.location.reload();
   };
   return (
     <div className="pb-4 w-[270px] bg-[#fff] absolute z-30 rounded-b-md shadow-sm">
