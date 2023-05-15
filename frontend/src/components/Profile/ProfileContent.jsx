6be93cec997d9a976dc5bf2b91e5ea2f975ca246
Felix-fale <falefelix23@gmail.com>
2023-05-15 15:18:19 +0000

    feat: add TrackOrder component content

--- frontend/src/components/Profile/ProfileContent.jsx
+++ frontend/src/components/Profile/ProfileContent.jsx
@@ -213,7 +213,11 @@ function ProfileContent({ active }) {
       )}
 
       {/* Track order */}
-      {active === 5 && <div>{/* <TrackOrder /> */}</div>}
+      {active === 5 && (
+        <div>
+          <TrackOrder />
+        </div>
+      )}
 
       {/* Change Password */}
       {active === 6 && <div>{/* <ChangePassword /> */}</div>}
@@ -438,6 +442,114 @@ const AllRefundOrders = () => {
         pageSize={10}
         autoHeight
         disableSelectionOnClick
+      />
+    </div>
+  );
+};
+
+const TrackOrder = () => {
+  const { user } = useSelector((state) => state.user);
+  // const { orders } = useSelector((state) => state.order);
+  const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(getAllOrdersOfUser(user._id));
+  // }, []);
+
+  const orders = [
+    {
+      _id: "74555554646fefdgddssf",
+      orderItems: [
+        {
+          name: "Iphone 14 pro max",
+        },
+      ],
+      totalPrice: 120,
+      orderStatus: "Processing",
+    },
+    {
+      _id: "74555554646fefdgdddssf",
+      orderItems: [
+        {
+          name: "Iphone 14 prox max",
+        },
+      ],
+      totalPrice: 1200,
+      orderStatus: "Procexssing",
+    },
+  ];
+
+  const columns = [
+    { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: (params) => {
+        return params.getValue(params.id, "status") === "Delivered"
+          ? "greenColor"
+          : "redColor";
+      },
+    },
+    {
+      field: "itemsQty",
+      headerName: "Items Qty",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+
+    {
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.8,
+    },
+
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 150,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/user/track/order/${params.id}`}>
+              <Button>
+                <MdTrackChanges size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+
+  const row = [];
+
+  orders &&
+    orders.forEach((item) => {
+      row.push({
+        id: item._id,
+        itemsQty: item.orderItems.length,
+        total: "US$ " + item.totalPrice,
+        status: item.orderStatus,
+      });
+    });
+
+  return (
+    <div className="pl-8 pt-1">
+      <DataGrid
+        rows={row}
+        columns={columns}
+        pageSize={10}
+        disableSelectionOnClick
+        autoHeight
       />
     </div>
   );
