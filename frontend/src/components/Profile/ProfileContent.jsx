a6f6954b06d02a0443609e7e1472069885c23e43
Felix-fale <falefelix23@gmail.com>
2023-05-15 10:20:55 +0000

    feat: install @material-ui/data-grid @material-ui/core

--- frontend/src/components/Profile/ProfileContent.jsx
+++ frontend/src/components/Profile/ProfileContent.jsx
@@ -7,8 +7,8 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import { backend_url, server } from "../../server";
 import styles from "../../styles/styles";
-// import { DataGrid } from "@material-ui/data-grid";
-// import { Button } from "@material-ui/core";
+import { DataGrid } from "@material-ui/data-grid";
+import { Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import { MdTrackChanges } from "react-icons/md";
 import { RxCross1 } from "react-icons/rx";
@@ -198,7 +198,11 @@ function ProfileContent({ active }) {
       )}
 
       {/* order */}
-      {active === 2 && <div>{/* <AllOrders /> */}</div>}
+      {active === 2 && (
+        <div>
+          <AllOrders />
+        </div>
+      )}
 
       {/* Refund */}
       {active === 3 && <div>{/* <AllRefundOrders /> */}</div>}
@@ -214,5 +218,103 @@ function ProfileContent({ active }) {
     </div>
   );
 }
+
+const AllOrders = () => {
+  // const { user } = useSelector((state) => state.user);
+  // const { orders } = useSelector((state) => state.order);
+  const dispatch = useDispatch();
+
+  const orders = [
+    {
+      _id: "74555554646fefdgddssf",
+      orderItems: [
+        {
+          name: "Iphone 14 pro max",
+        },
+      ],
+      totalPrice: 120,
+      orderStatus: "Processing",
+    },
+  ];
+
+  // useEffect(() => {
+  //   dispatch(getAllOrdersOfUser(user._id));
+  // }, []);
+
+  const columns = [
+    { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: (params) => {
+        return params.getValue(params.id, "status") === "Delivered"
+          ? "greenColor"
+          : "redColor";
+      },
+    },
+    {
+      field: "itemsQty",
+      headerName: "Items Qty",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+
+    {
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.8,
+    },
+
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 150,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/user/order/${params.id}`}>
+              <Button>
+                <AiOutlineArrowRight size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+
+  const row = [];
+
+  orders &&
+    orders.forEach((item) => {
+      row.push({
+        id: item._id,
+        itemsQty: item.cart.length,
+        total: "US$ " + item.totalPrice,
+        status: item.status,
+      });
+    });
+
+  return (
+    <div className="pl-8 pt-1">
+      <DataGrid
+        rows={row}
+        columns={columns}
+        pageSize={10}
+        disableSelectionOnClick
+        autoHeight
+      />
+    </div>
+  );
+};
 
 export default ProfileContent;
