820bd2c131a1f19f44f5571d180fc17da7af2b8d
Felix-fale <falefelix23@gmail.com>
2023-05-19 21:10:04 +0000

    feat: test ShopCreates component logic

--- frontend/src/components/Shop/ShopCreates.jsx
+++ frontend/src/components/Shop/ShopCreates.jsx
@@ -176,7 +176,7 @@ function ShopCreates(props) {
                   onChange={(e) => setPassword(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 />
-                {visible ? (
+                {/* {visible ? (
                   <AiOutlineEye
                     className="absolute right-2 top-2 cursor-pointer"
                     size={25}
@@ -188,7 +188,7 @@ function ShopCreates(props) {
                     size={25}
                     onClick={() => setVisible(true)}
                   />
-                )}
+                )} */}
               </div>
             </div>
 
